1734939def98dd160f0fa9e35b1c6139fae57489
OmarES <omar.ohm.elsherif@gmail.com>
2021-06-13 15:45:36 +0200

    feat: set up basic authentication & authorization

--- backend/model/User.js
+++ backend/model/User.js
@@ -1,13 +1,49 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// this one is really basic!
 const UserSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     username: {
         type: String,
-        required: true
-    }
+        required: [true, "Please provide a username"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
 });
+
+UserSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+UserSchema.methods.matchPasswords = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+UserSchema.methods.getSignedToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE })
+}
 
 const User = mongoose.model('User', UserSchema);
 
