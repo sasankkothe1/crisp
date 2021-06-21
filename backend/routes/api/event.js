6558b418415eb45275a1f7abe45b7e1c44b706a9
OmarES <omar.ohm.elsherif@gmail.com>
2021-06-21 15:18:09 +0200

    Fixed formatting inconsistencies

--- backend/routes/api/event.js
+++ backend/routes/api/event.js
@@ -18,15 +18,28 @@ const mimetypes = [
 
 const upload = require("../../middleware/upload")(mimetypes);
 
-
-router.post("/", isAuthenticated, upload.array("media"), EventController.create);
+router.post(
+    "/",
+    isAuthenticated,
+    upload.array("media"),
+    EventController.create
+);
 router.get("/test", EventController.test);
 router.get("/", EventController.listEvents);
 router.get("/new", EventController.listNewEvents);
-router.get("/postedBy/:id", isAuthenticated, EventController.listEventsByUserID);
+router.get(
+    "/postedBy/:id",
+    isAuthenticated,
+    EventController.listEventsByUserID
+);
 router.get("/soonEnding", EventController.listSoonEndingEvents);
 router.get("/:id", isAuthenticated, EventController.read);
-router.put("/:id", isAuthenticated, upload.array("media"), EventController.update);
+router.put(
+    "/:id",
+    isAuthenticated,
+    upload.array("media"),
+    EventController.update
+);
 router.delete("/:id", isAuthenticated, EventController.remove);
 
 module.exports = router;
--- backend/routes/api/recipe.js
+++ backend/routes/api/recipe.js
@@ -19,13 +19,27 @@ const mimetypes = [
 
 const upload = require("../../middleware/upload")(mimetypes);
 
-router.post("/", isAuthenticated, upload.array("media"), RecipeController.create);
+router.post(
+    "/",
+    isAuthenticated,
+    upload.array("media"),
+    RecipeController.create
+);
 router.get("/", RecipeController.listRecipes);
 router.get("/new", RecipeController.listNewRecipes);
-router.get("/postedBy/:id", isAuthenticated, RecipeController.listRecipesByUserID);
+router.get(
+    "/postedBy/:id",
+    isAuthenticated,
+    RecipeController.listRecipesByUserID
+);
 router.get("/:cuisine", RecipeController.listRecipesByCuisine);
 router.get("/:id", isAuthenticated, RecipeController.read);
-router.put("/:id", isAuthenticated, upload.array("media"), RecipeController.update);
+router.put(
+    "/:id",
+    isAuthenticated,
+    upload.array("media"),
+    RecipeController.update
+);
 router.delete("/:id", isAuthenticated, RecipeController.remove);
 
 module.exports = router;
