0c0b8cd75ca5af9aeed2d8baae68123a0b13bc2f
OmarES <omar.ohm.elsherif@gmail.com>
2021-06-27 13:11:30 +0200

    Add post creation update to creator user document

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -16,7 +16,7 @@ const create = async (req, res) => {
     let url = req.protocol + "://" + req.get("host") + "/";
     let mediaFiles = [];
 
-    if (req.files.length > 0) {
+    if (req.files?.length > 0) {
         for (var i = 0; i < req.files.length; i++)
             mediaFiles.push(url + req.files[i].filename);
     }
@@ -25,14 +25,38 @@ const create = async (req, res) => {
         media: mediaFiles,
     };
 
+    const session = await PostModel.startSession();
+    session.startTransaction();
+
     PostModel.create(post)
-        .then((post) => res.status(201).json(post))
-        .catch((error) =>
+        .then((post) => {
+            req.user.posts.push(post._id);
+            req.user.save(async (error) => {
+                if (error) {
+                    throw error;
+                } else {
+                    try {
+                        await session.commitTransaction();
+                        res.status(201).json(post);
+                    } catch (error) {
+                        res.status(500).json({
+                            error: "Internal server error",
+                            message: error.message,
+                        });
+                    } finally {
+                        session.endSession();
+                    }
+                }
+            });
+        })
+        .catch(async (error) => {
+            await session.abortTransaction();
+            session.endSession();
             res.status(500).json({
                 error: "Internal server error",
                 message: error.message,
-            })
-        );
+            });
+        });
 };
 
 const listPosts = (req, res) => {
