c9021ae0547cd6d18f33791ff1696391c0670c30
OmarES <omar.ohm.elsherif@gmail.com>
2021-07-03 15:26:54 +0200

    Add basic header skeleton
    Co-authored-by: sasankkothe1 <sasankkothe1@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/Header";
+//import Header2 from "./components/Header2/Header2";
+
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Header />
+            </div>
+        </BrowserRouter>
     );
 }
 
