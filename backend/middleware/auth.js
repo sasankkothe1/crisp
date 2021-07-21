638b7e38b583f26dbf2c804cb916270a7efc2918
Venkata Kothe <ga53xil@mytum.de>
2021-07-21 12:47:55 +0000

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -21,7 +21,7 @@ exports.isAuthenticated = async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const user = await User.findById(decoded.id);
+        const user = await User.findById(decoded._id);
 
         if (!user) {
             return next(new ErrorResponse("No user found with this id", 404));
